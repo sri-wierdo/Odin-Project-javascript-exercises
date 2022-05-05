47f6c97b7bdc5480b06535f63b6e9561e72fe493
sri_wierdo <mannemsrivishnureddy@gmail.com>
2022-05-05 22:38:32 +0530

    Do leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    console.log(typeof year)
+    if(year % 4 == 0 && typeof year === "number" ){
+        if(year %100 == 0 && year % 400 != 0){
+            return false;
+        } else return true;
+    }else return false;
 };
 
+leapYears(2000)
 // Do not edit below this line
 module.exports = leapYears;
