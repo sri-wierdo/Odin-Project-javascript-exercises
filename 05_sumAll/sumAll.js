2d2e6a3695a85d0bdc25183e96a071c0a974f139
sri_wierdo <mannemsrivishnureddy@gmail.com>
2022-05-05 22:27:22 +0530

    Do sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1 , num2) {
+    let sumAll = 0;
+    if(num1 > 0 && num2 > 0 && typeof num1 === "number" && typeof num2 === "number"){
+    if(num1 < num2){
+    for(let i = num1 ; i<= num2 ; i++){
+        sumAll = sumAll+i;
+        }
+    }
+    else if(num1 > num2){
+        for(let i = num2 ; i<= num1 ; i++){
+            sumAll = sumAll+i;
+            }
+
+    }
+
+    return sumAll
+}
+else{return "ERROR";}
+    
 
 };
 
