6b688b8b8165e64c622b4582dc9792c726313a05
sri_wierdo <mannemsrivishnureddy@gmail.com>
2022-05-06 22:03:40 +0530

    Do fibonacci exercise in js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+if(typeof number === "string"){
+    number = parseInt(number)
+}
+if(number < 0){
+    return "OOPS"
+}
+
+let fibbunacci = [];
+let a = 0;
+let b = 1;
+let c;
+fibbunacci.push(b) ;
+for(let i = 2 ; i<=number ; i++){
+c = a + b;
+fibbunacci.push(c);
+a = b ;
+b = c ;
+}
+return fibbunacci[fibbunacci.length -1]
 
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
