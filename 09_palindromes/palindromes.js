bce086fe7088a3291cb811ca08750e9a956c3cb3
sri_wierdo <mannemsrivishnureddy@gmail.com>
2022-05-06 22:28:59 +0530

    Do palindrome Exercise in js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^\w\s\']|_/g, "")
+    let temp = str.split(" ");
+    temp = temp.join("");
+    temp = temp.toLowerCase();
+    let temp2 = temp.split("");
+    temp2 = temp2.reverse()
+    temp2 = temp2.join("")
+    if(temp == temp2){
+        return true;
+    }else{return false}
 
 };
 
